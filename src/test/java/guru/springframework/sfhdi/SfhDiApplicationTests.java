63ee7a1e6f32fe8be4885431d0677b73d3b7e943
Victor Herrera <vherrera@redpoints.n.om>
2023-02-06 13:02:41 +0100

    Initial commit

---
+++ src/test/java/guru/springframework/sfhdi/SfhDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfhdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfhDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
